8248fb806a81b7c328dc3be7418ec74937625fa2
himanshu7437 <himanshujangra368@gmail.com>
2025-08-07 07:20:22 +0530

    p2 completed

---
+++ DOM/project2/script.js
@@ -0,0 +1,15 @@
+var container = document.querySelector("#container");
+var love = document.querySelector("i");
+
+container.addEventListener("dblclick", function () {
+  love.style.transform = "translate(-50%, -50%) scale(1)";
+  love.style.opacity = 0.8;
+
+  setTimeout(() => {
+    love.style.opacity = 0;
+  }, 1000);
+  
+  setTimeout(() => {
+    love.style.transform = "translate(-50%, -50%) scale(0)";
+  }, 2000);
+});
