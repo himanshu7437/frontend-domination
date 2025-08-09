61c1ef67ed03de21d91cd413c4faaf26ac43baff
himanshu7437 <himanshujangra368@gmail.com>
2025-08-09 22:12:18 +0530

    project 2 added

---
+++ DOM/project2/script.js
@@ -0,0 +1,13 @@
+// var followbtn = document.querySelector('#follow-btn')
+// var removebtn = document.querySelector('#remove-btn')
+
+// followbtn.addEventListener('click', function(){
+//     followbtn.innerHTML = 'following'
+//     followbtn.style.backgroundColor = 'green'
+
+// })
+
+// removebtn.addEventListener('click', function(){
+//     followbtn.style.backgroundColor = 'lemonchiffon'
+//     followbtn.innerHTML = 'follow'
+// })
