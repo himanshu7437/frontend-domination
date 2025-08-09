d79df228db8b2cd3de318486520372fadde58acd
himanshu7437 <himanshujangra368@gmail.com>
2025-08-09 21:55:34 +0530

    Dom on of bulb project added

---
+++ DOM/project1/script.js
@@ -0,0 +1,23 @@
+
+
+// bulb glow code
+
+
+// var bulb = document.querySelector(".circle")
+
+// var btn1 = document.querySelector(".btn1")
+// var invert = 1;
+
+// btn1.addEventListener("click", function(){
+//    if(invert == 1) {
+//     bulb.style.backgroundColor = 'yellow';
+//     btn1.innerHTML = 'OFF'
+//     invert = 0;
+//    } else {
+//     bulb.style.backgroundColor = 'white';
+//     btn1.innerHTML = 'ON'
+//     invert = 1;
+//    }
+// }
+// )
+
