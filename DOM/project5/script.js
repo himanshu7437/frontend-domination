958c6910b138a4caa458cfd3c64c2668dd5fb961
himanshu7437 <himanshujangra368@gmail.com>
2025-08-16 09:29:45 +0530

    Dom project 5 done

---
+++ DOM/project5/script.js
@@ -0,0 +1,9 @@
+// questions
+// create a html page with a btn. when the btn is clicked change the text of the paragraph element.
+
+var btn = document.querySelector('button');
+var p = document.querySelector('p');
+
+btn.addEventListener('click', function() {
+    p.textContent = "Clicked";
+})
