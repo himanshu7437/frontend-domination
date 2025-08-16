a6ccb3fee61aa37d31398b6e3f4ff0614aafa63b
himanshu7437 <himanshujangra368@gmail.com>
2025-08-16 09:16:51 +0530

    intermediate completed

--- js/basics/intermediate/script.js
+++ js/basics/intermediate/script.js
@@ -50,3 +50,96 @@
 // 3. lexcal enviroment 
 // var - acess all child 
 // let - cdef 
+
+// 7) how to copy a refrence 
+
+// array 
+// var arr = [1, 3, 4, 5, 6];
+// var grr = [...arr];
+// grr[0] = 10000;
+
+// object 
+
+// var obj = {
+//     name: 'ashish',
+// }
+
+// var ggg = {...obj}
+
+// ggg.class = 34;
+
+// 8) truthy and falsy 
+
+// if(false) {
+//     console.log('true');
+    
+// } else {
+//     console.log('false');
+// }
+
+// truthy - remaining
+// falsy - null, undefined, NaN, 0, false, document.all
+
+// 9) switch - use for conditional 
+// var month = 2
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+
+//     default:
+//         console.log("default value");
+//         break;
+// }
+
+// 10) for each  - array p jb loop lgake har ek element p kuch kaam krna ho
+
+// var arr = [1 , 2, 3, 4, 5, 6];
+
+// var hello = arr.forEach(function(num) {
+//     console.log(num+1);
+// })
+
+// -- for in - object p jb loop lgana ho
+
+// var obj = {
+//     name: 'ankit',
+//     class: 2,
+//     college: 'vaish',
+//     city: 'bhiwani'
+// }
+
+// for (var key in obj) {
+//     console.log(key, obj[key]);
+// }
+
+// 11) how array are made - in js arrays are converted into objects.
+
+// var arr = [11, 2, 4, 5];
+
+// arr[-1] = 23;
+// var arr = {
+//     0: 11,
+//     1: 2,
+//     3: 5
+// }
+
+// 12) delete a prop in object 
+
+//  var obj = {
+//     name: 'ankit',
+//     class: 2,
+//     college: 'vaish',
+//     city: 'bhiwani'
+// }
+
+// delete obj.college;
+
+// obj.college = 'vaish';
+// obj.name = 'vaish';
