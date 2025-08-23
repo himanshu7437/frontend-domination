a5d8a12c6a7c9466acf14945905df3dd06b448a0
himanshu7437 <himanshujangra368@gmail.com>
2025-08-23 14:55:22 +0530

    solutions addedd

--- js/interview-questions/02/script.js
+++ js/interview-questions/02/script.js
@@ -3,13 +3,38 @@
 // -------------------  console log ---------------------
 
 // 1. log your name and favourite obby to console.
+
+// console.log("himanshu sharma");
+// console.log("reading books, playing games");
+
+
 // 2. perform and log the result - 45*2-10
+
+// console.log(45*2-10);
+
 // 3. console current year
+
+// var newd = new Date();
+// console.log(newd.getFullYear());
+
 // 4. create two variable for first and last name , concatenate them and log them
-// 5. trackk the value of a variable byh logging it before and after update 
+
+// var first = "Himanshu";
+// var last = " Sharma";
+// console.log(first+last);
+
+// 5. trackk the value of a variable by logging it before and after update 
+
+// var a = 10;
+// console.log(a);
+// a = 12;
+// console.log(a);
+
+
 // 6. use console.error() to simulate an error message 
 // 7. log the sqauare of the number 12 to the console
 // 8. print the type of a variable holding a value true
 // 9. make a variable holding your age and log whether it id greater than 18.
 // 10. log the result of 100/0 and observe the output.
 
+// -------------------  variable and datatypes ---------------------
