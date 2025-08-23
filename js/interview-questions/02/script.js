e6184469d25ac420d7eb0c1eb3fd68d6f786301b
himanshu7437 <himanshujangra368@gmail.com>
2025-08-23 14:47:42 +0530

    10 questions added on console

---
+++ js/interview-questions/02/script.js
@@ -0,0 +1,15 @@
+// 50 interview Questions
+
+// -------------------  console log ---------------------
+
+// 1. log your name and favourite obby to console.
+// 2. perform and log the result - 45*2-10
+// 3. console current year
+// 4. create two variable for first and last name , concatenate them and log them
+// 5. trackk the value of a variable byh logging it before and after update 
+// 6. use console.error() to simulate an error message 
+// 7. log the sqauare of the number 12 to the console
+// 8. print the type of a variable holding a value true
+// 9. make a variable holding your age and log whether it id greater than 18.
+// 10. log the result of 100/0 and observe the output.
+
