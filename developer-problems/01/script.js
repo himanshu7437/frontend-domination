d746f012d749f1fb9ed2ae935a42cfb6a754181e
himanshu7437 <himanshujangra368@gmail.com>
2025-08-24 18:59:44 +0530

    type error problem

--- developer-problems/01/script.js
+++ developer-problems/01/script.js
@@ -1,1 +1,10 @@
-// 
+// developer problem - 1
+// debugging a type error: cannot read property of "x"
+
+// var a = {};
+// a.name.first; // error -> Uncaught TypeError: Cannot read properties of undefined (reading 'first')
+// reason ->  this occurs because we are aceesing a property which doesnt exist.
+
+var a = {};
+// solution :- use conditionals.
+a?.name?.first; 
