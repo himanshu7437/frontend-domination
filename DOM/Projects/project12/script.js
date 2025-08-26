71a1a72f5ae0e706f4626cdc2d30564504f274b5
himanshu7437 <himanshujangra368@gmail.com>
2025-08-26 07:41:10 +0530

    half done project 12

--- DOM/Projects/project12/script.js
+++ DOM/Projects/project12/script.js
@@ -1,3 +1,41 @@
 // create a search bat that siplays live search results as users type, updating the result without requiring a full page reload
 
- 
+ const data = [
+    {name: "hiamsnhu", src: "https://images.pexels.com/photos/33516862/pexels-photo-33516862.jpeg"},
+    {name: "hello", src: "https://images.pexels.com/photos/33516862/pexels-photo-33516862.jpeg"},
+    {name: "hiamsnhu", src: "https://images.pexels.com/photos/33516862/pexels-photo-33516862.jpeg"},
+    {name: "hiamsnhu", src: "https://images.pexels.com/photos/33516862/pexels-photo-33516862.jpeg"},
+    {name: "hiamsnhu", src: "https://images.pexels.com/photos/33516862/pexels-photo-33516862.jpeg"},
+ ]
+
+ var prsnss = ""
+ data.forEach(function(elem) {
+    prsnss += `<div class="person">
+                    <div class="img">
+                        <img src="${elem.src}" alt="">
+                    </div>
+                    <h4>${elem.name}</h4>
+                </div>`
+    document.querySelector(".people").innerHTML = prsnss;
+
+ })
+
+ var inputt =  document.querySelector("input")
+
+inputt.addEventListener("input", function() {
+    var matchingData = data.filter(function(e) {
+        return e.name.startsWith(inputt.value)
+    })
+
+    const {name, src} = matchingData[0]
+    matchingData.forEach(function(){
+        prsnss += `<div class="person">
+                    <div class="img">
+                        <img src="${src}" alt="">
+                    </div>
+                    <h4>${name}</h4>
+                </div>`
+            })            
+            
+            document.querySelector(".people").innerHTML = prsnss;
+        })
