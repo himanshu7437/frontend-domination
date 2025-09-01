92db2f6df93e7e58a79e22a89fac2e26f23aaf4e
himanshu7437 <himanshujangra368@gmail.com>
2025-09-01 09:45:55 +0530

    functions questions added

--- js/interview-questions/04/script.js
+++ js/interview-questions/04/script.js
@@ -48,3 +48,81 @@
 
 
 // -------------------------------functions ---------------------------------
+
+// 1. write a js function to reverse a number?
+
+
+// ist method 
+// function reverseanum(num) {
+//     var result = Number(num.toString().split("").reverse().join(""));    
+//     return result;
+// }
+
+// var a = 1234;
+// console.log(reverseanum(a));
+
+// 2nd method 
+
+// function reversenum(num) {
+//     var rev = 0;
+//     while (num > 0) {
+//         rev = rev*10 + num%10;
+//         num = Math.floor(num/10);
+//     } 
+//     return rev;
+// }
+
+
+// var a = 234345;
+// console.log(reversenum(a));
+
+// 2. write a function to check if a given string is palindrome or not?
+
+// function checkPalindrome(str) {
+//     return str.split("").reverse().join("") === str
+// }
+
+// var a = "sos"
+// console.log(checkPalindrome(a));
+
+// 3. write a js fn that returns a paassed string with letters in alphbetical order?
+
+// function alphaorder(str) {
+//     return str.split(" ").map(function(elem) {
+//         return elem.split("").sort().join("");
+//     }).join(" ");
+// }
+// var str = "hello i am himanshu"
+// console.log(alphaorder(str));
+
+// 4. write a js fn which accept a string as a parameter and converts the first letters of each word in the uppercase?
+
+// function capitalize(str) {
+//     return str.split(" ").map(function(elem){
+//         return elem.charAt(0).toUpperCase() + elem.substring(1)
+//     }).join(" ");
+// }
+// var  a = "hey this ia a cat"
+// console.log(capitalize(a));
+
+// 5. write a js fn to get the no. of occurece of each letter in a a specific string?
+
+// function checkOccur(str) {
+//     var newstr = str.replaceAll(" ", "")
+//     var a = {};
+//     for(let i = 0; i < newstr.length; i++){
+//         if(a.hasOwnProperty(newstr.charAt(i))) {
+//             a[newstr.charAt(i)]++;
+//         } else {
+//             a[newstr.charAt(i)] = 1;
+//         }
+//     }
+//     return a;
+// }
+
+// var a = "hey am i frontend developer";
+
+// console.log(checkOccur(a));
+
+
+
