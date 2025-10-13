6af20bcfba5e55a8cc103027abbcc45e5d4f2909
himanshu7437 <himanshujangra368@gmail.com>
2025-10-13 23:47:12 +0530

    feat: add initial weather app structure with HTML, CSS, and JavaScript

---
+++ website-projects/weather-app/script.js
@@ -0,0 +1,30 @@
+let temper = document.querySelector('#tempp');
+let humidity = document.querySelector('#humidity');
+let windspeed = document.querySelector('#wind-speed');
+let city = document.querySelector('#cityy');
+let inputtbtn = document.querySelector('.input-area button');
+let inputt = document.querySelector('.input-area input');
+
+let weathercity = "New Delhi"
+
+async function apicall(param) {
+    try {
+        // let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&appid={apikey}`);
+        let data = await response.json();
+        temper.textContent = `${Math.floor(data.main.temp - 273.15)}°C)`;
+        humidity.textContent = `${data.main.humidity}%`;
+        windspeed.textContent = `${data.wind.speed} km/hr`;
+        city.textContent = data.name;
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+inputtbtn.addEventListener("click", function(){
+    weathercity = inputt.value.trim();
+    console.log(weathercity);
+    apicall(weathercity);
+})
+
+apicall(weathercity);
