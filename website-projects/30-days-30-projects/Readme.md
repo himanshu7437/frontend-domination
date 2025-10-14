3fbf9661fbd50d9c69795c4b18bbd7aba53c8b74
himanshu7437 <himanshujangra368@gmail.com>
2025-10-14 14:45:58 +0530

    enhance weather app UI and functionality with input area, weather icon handling, and improved styles

---
+++ website-projects/30-days-30-projects/Readme.md
@@ -0,0 +1,2 @@
+# weather app
+here we are going to make function weather app to show case the weathwe info bt searching the input city.
--- website-projects/30-days-30-projects/weather-app/script.js
+++ website-projects/30-days-30-projects/weather-app/script.js
@@ -4,17 +4,28 @@ let windspeed = document.querySelector('#wind-speed');
 let city = document.querySelector('#cityy');
 let inputtbtn = document.querySelector('.input-area button');
 let inputt = document.querySelector('.input-area input');
+let weatherimg = document.querySelector('.bottom img');
 
 let weathercity = "New Delhi"
 
+function getwethericon(weatherdets) {
+    if(weatherdets == "sunny"){
+        weatherimg.src = "./sunny.png"
+    } else if (weatherdets == "haze") {
+        weatherimg.src = "./haze.png"
+    }
+}
+  
 async function apicall(param) {
     try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=4b89e60279096d88a4fe22fede99a150`);
+        // let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&appid={apikey}`);
         let data = await response.json();
         temper.textContent = `${Math.floor(data.main.temp - 273.15)}°C)`;
         humidity.textContent = `${data.main.humidity}%`;
         windspeed.textContent = `${data.wind.speed} km/hr`;
         city.textContent = data.name;
+        console.log(data);
+        
         
     } catch (error) {
         console.log(error);
